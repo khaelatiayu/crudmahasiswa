4e764ea019c180dbabacacb7dbd8533e9fa12719
khaelatiayu <khaelatiayu@gmail.com>
2026-02-12 17:03:37 +0700

    update model dan controller

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,37 +1,7 @@
-{{-- resources/views/layouts/app.blade.php --}}
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Mahasiswa</title>
-    
-    {{-- Bootstrap 5 CSS --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-    {{-- Optional: Bootstrap Icons --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-</head>
-<body>
-    {{-- Navbar sederhana --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">Sistem Informasi Mahasiswa</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('mahasiswa.index') }}">Mahasiswa</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+{{-- resources/views/mahasiswa/index.blade.php --}}
+@extends('layouts.app')
 
-    {{-- Content --}}
+@section('content')
 <div class="container mt-4">
     <div class="row">
         <div class="col-md-12">
@@ -106,11 +76,3 @@
     </div>
 </div>
 @endsection
-    <main>
-        @yield('content')
-    </main>
-
-    {{-- Bootstrap 5 JS --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
